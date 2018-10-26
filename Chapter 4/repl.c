57128ef5135c1d5d3168faf8f0bab28498fc3747
AmyShackles <amyshackles@gmail.com>
2018-10-26 12:33:18 -0700

    Added preprocessor instructions for different OS

--- Chapter 4/repl.c
+++ Chapter 4/repl.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
+/* If we are compiling on Windows, compile these functions */
+#ifdef _WIN32
+#include <string.h>
 
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
 	/* Print Version and Exit Information */
@@ -11,6 +32,7 @@ int main(int argc, char** argv) {
 	
 	/* In a never-ending loop ... */
 	while (1) {
+		/* Now in either case readline will be defined */
 		/* Output our prompt and get input */
 		char* input = readline("lispy> ");
 		
