169c9b7f136e4e6f235f8c513948f863ab885a15
AmyShackles <amyshackles@gmail.com>
2018-10-26 11:29:35 -0700

    Removed fgets, replaced with editline functions

--- Chapter 4/repl.c
+++ Chapter 4/repl.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <editline/readline.h>
+
 
 int main(int argc, char** argv) {
 	/* Print Version and Exit Information */
@@ -10,14 +11,17 @@ int main(int argc, char** argv) {
 	
 	/* In a never-ending loop ... */
 	while (1) {
-		/* Output our prompt */
-		fputs("lispy> ", stdout);
+		/* Output our prompt and get input */
+		char* input = readline("lispy> ");
 		
-		/* Read a line of user input of maximum size 2048 */
-		fgets(input, 2048, stdin);
+		/* Add input to history */
+		add_history(input);
 		
 		/* Echo input back to user */
-		printf("No you're a %s", input);
+		printf("No you're a %s\n", input);
+		
+		/* Free retrieved input */
+		free(input);
 	}
 	return 0;
 }
