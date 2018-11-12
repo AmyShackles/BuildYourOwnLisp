040c18d01c64e00a4ee8e60bb530d6b559f711e3
AmyShackles <amyshackles@gmail.com>
2018-11-12 11:06:31 -0800

    Changed lval_eval_sexpr function to use lval_call

--- Chapter 12/functions.c
+++ Chapter 12/functions.c
@@ -295,3 +295,28 @@ lval* lval_call(lenv* e, lval* f, lval* a) {
 		return lval_copy(f);
 	}
 }
+
+lval* lval_eval_sexpr(lenv* e, lval* v) {
+	for(int i = 0; i < v->count; i++) {
+		v->cell[i] = lval_eval(e, v->cell[i]);
+	}
+	
+	for (int i = 0; i < v->count; i++) {
+		if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+	}
+	
+	if (v->count == 0) { return v; }
+	if (v->count == 1) { return lval_take(v, 0); }
+	
+	lval* f = lval_pop(v, 0);
+	if (f->type != LVAL_FUN) {
+		lval* err = lval_err(
+			"S-Expression starts with incorrect type. "
+			"Got %s, Expected %s.",
+			ltype_name(f->type), ltype_name(LVAL_FUN);
+		lval_del(f); lval_del(v);
+		return err;
+	}
+	
+	lval* result = lval_call(e, f, v);
+}
